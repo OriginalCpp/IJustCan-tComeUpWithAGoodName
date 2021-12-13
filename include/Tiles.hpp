5641035b2f20fd086a9e9f95c9a58b4b86e2744d
Martin <martinilgner22@gmail.com>
2021-12-13 17:24:03 +0100

    made tiles in 2d vector

---
+++ include/Tiles.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "GameObject.hpp"
+#include "SDL.h"
+
+class Tile :public GameObject
+{
+    bool collidable {};
+public:
+    Tile(SDL_Texture* p_texture, SDL_Rect p_src, SDL_Rect p_dst, bool p_collidable = true);
+};
---
+++ src/tiles.cpp
@@ -0,0 +1,10 @@
+#include "Tiles.hpp"
+#include "GameObject.hpp"
+#include "SDL.h"
+
+
+Tile::Tile(SDL_Texture* p_texture, SDL_Rect p_src, SDL_Rect p_dst, bool p_collidable)
+    :GameObject(p_texture, p_src, p_dst)
+{
+    collidable = p_collidable;
+}
